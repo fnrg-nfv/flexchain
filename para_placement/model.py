b4a8f0147d7ae375e5cecffd512e18a8548838df
virgilma <virgilma28@gmail.com>
2019-07-15 18:15:13 +0800

    Merge Configuration into Model

--- para_placement/model.py
+++ para_placement/model.py
@@ -1,11 +1,12 @@
+import heapq
 import pickle
 import random
+import string
 from typing import List
 import networkx as nx
 import matplotlib.pyplot as plt
 
 from para_placement import topology
-from para_placement.solution import Configuration
 
 
 class BaseObject(object):
@@ -116,13 +117,11 @@ def generate_sfc_list(topo: nx.Graph, size=100):
         n = random.randint(5, 10)
         vnf_list = []
         for j in range(n):
-            # TODO: the latency of VNF could be larger, and the computing_resource is very important
             vnf_list.append(VNF(latency=random.uniform(0.2, 2), computing_resource=random.randint(400, 800)))
         s = random.randint(1, nodes_len - 1)
         d = random.randint(1, nodes_len - 1)
         while d == s:
             d = random.randint(1, nodes_len - 1)
-        # TODO: the throughput requirement is very important
         ret.append(SFC(vnf_list, latency=random.randint(10, 30), throughput=random.randint(100, 1000), s=s, d=d, idx=i))
     return ret
 
@@ -131,6 +130,224 @@ def generate_model(topo_size: int = 100, sfc_size: int = 100) -> Model:
     topo = topology.generate_randomly(topo_size)
     sfc_list = generate_sfc_list(topo, sfc_size)
     return Model(topo, sfc_list)
+
+
+class Configuration(BaseObject):
+    def __init__(self, sfc: SFC, route: List[int], place: List[int], route_latency: int, idx: string):
+        self.sfc = sfc
+        self.route = route
+        self.place = place
+        self._route_latency = route_latency
+        # self.latency = route_latency + sfc.vnf_latency_sum
+        self.name = "{}_{}".format(sfc.idx, idx)
+
+        # computing resource
+        self.computing_resource = {}
+        for i, vnf in enumerate(sfc.vnf_list):
+            pos = route[place[i]]
+            if pos not in self.computing_resource:
+                self.computing_resource[pos] = 0
+            self.computing_resource[pos] += vnf.computing_resource
+
+        # throughput
+        self.edges = []
+        for i in range(len(route) - 1):
+            start = max(route[i], route[i + 1])
+            end = min(route[i], route[i + 1])
+            self.edges.append("%d:%d" % (start, end))
+
+        self.l = 9999999  # used to find optimal situation
+
+    def __str__(self):
+        return "route: {}\tplace: {}\tcomputing_resource: {}".format(self.route.__str__(), self.place.__str__(),
+                                                                     self.computing_resource.__str__())
+
+    def get_latency(self, para: bool = False) -> float:
+        if para:
+            return 1  # todo
+
+        return self._route_latency + self.sfc.vnf_latency_sum
+
+    def computing_resource_ratio(self, topo: nx.Graph) -> float:
+        ret = 0
+        for pos in self.computing_resource:
+            ret = max(self.computing_resource[pos] / topo.nodes.data()[pos]['computing_resource'], ret)
+        return ret
+
+    def para_latency_analysis(self) -> float:
+        """
+        Get the optimal parallel execution situation using backtracking
+        """
+        optimal_para = []
+        vnfs = [self.sfc.vnf_list[0]]
+        for i in range(len(self.place) - 1):
+            if self.place[i] == self.place[i + 1]:
+                vnfs.append(self.sfc.vnf_list[i + 1])
+            else:
+                if len(vnfs) > 1:
+                    result = []
+                    self.l = 999999
+                    self.backtracking_analysis(0, vnfs, [], result)
+                    optimal_para.extend(result)
+                    optimal_para.append(0)
+                    vnfs = [self.sfc.vnf_list[i + 1]]
+                else:
+                    optimal_para.append(0)
+                    vnfs = [self.sfc.vnf_list[i + 1]]
+        if len(vnfs) > 1:
+            result = []
+            self.l = 999999
+            self.backtracking_analysis(0, vnfs, [], result)
+            optimal_para.extend(result)
+        return optimal_para
+
+    def backtracking_analysis(self, index, vnfs, analysis_result, result):
+        """
+        for vnf in vnfs, find the optimal parallelism situation and save result in result.
+        len(result) = len(vnfs) - 1
+        result[i] indicates vnfs[i] and vnfs[i+1] whether execute parallelism. 0 for no, 1 for yes
+        """
+        if index == len(vnfs) - 1:
+            # analysis end, compute latency
+            l = 0
+            for vnf in vnfs:
+                l = l + vnf.latency
+            if l < self.l:
+                # find a better situation
+                self.l = l
+                result.clear()
+                result.extend(analysis_result)
+            return
+        if VNF.parallelizable_analysis(vnfs[index], vnfs[index + 1]) >= 0:
+            # parallelizable
+            vnf1 = vnfs.pop(index)
+            vnf2 = vnfs.pop(index)
+            new_vnf = VNF(max(vnf1.latency, vnf2.latency),
+                          vnf1.computing_resource + vnf2.computing_resource,
+                          set.union(vnf1.read_fields, vnf2.read_fields),
+                          set.union(vnf1.write_fields, vnf2.write_fields))
+            vnfs.insert(index, new_vnf)
+            analysis_result.append(1)
+            self.backtracking_analysis(index, vnfs, analysis_result, result)
+            vnfs.pop(index)
+            vnfs.insert(index, vnf2)
+            vnfs.insert(index, vnf1)
+            analysis_result.pop()
+            analysis_result.append(0)
+            self.backtracking_analysis(index + 1, vnfs, analysis_result,
+                                       result)
+            analysis_result.pop()
+        else:
+            analysis_result.append(0)
+            self.backtracking_analysis(index + 1, vnfs, analysis_result,
+                                       result)
+            analysis_result.pop()
+
+        # find the shortest distance to every point
+
+
+def dijkstra(topo: nx.Graph, s: int) -> {}:  # todo
+    ret = {}
+    heap = [(0, s)]
+    while heap:
+        distance, node = heapq.heappop(heap)
+        if node not in ret:
+            ret[node] = distance
+
+        adj_nodes = topo[node]
+        for node in adj_nodes:
+            if node not in ret:
+                new_distance = adj_nodes[node]['latency'] + distance
+                heapq.heappush(heap, (new_distance, node))
+
+    print("Dijkstra: {}".format(ret))
+    return ret
+
+
+# dfs + latency constraint (+ dijkstra)
+def generate_route_list(topo: nx.Graph, sfc: SFC):
+    s = sfc.s
+    d = sfc.d
+
+    shortest_distance = dijkstra(topo, d)
+    if s not in shortest_distance:
+        return []
+
+    stack = [([s], 0)]
+
+    route_set = []
+
+    while stack:
+        route, latency = stack.pop()
+
+        if latency + shortest_distance[route[-1]] > sfc.latency:  # partial latency constraints
+            continue
+
+        if route[-1] == d:
+            route_set.append((route, latency))
+        else:
+            adjacent = topo[route[-1]]
+            for index in adjacent:
+                if index not in route:
+                    new_route = route[:]
+                    new_route.append(index)
+                    stack.append(
+                        (new_route, latency + adjacent[index]['latency']))
+
+    print("Size of path set: %d" % len(route_set))
+
+    return route_set
+
+
+# bfs
+def generate_configuration(topo: nx.Graph, route: List[int], route_latency: int, sfc: SFC, idx: int) \
+        -> List[Configuration]:
+    m = len(sfc.vnf_list)
+    n = len(route)
+
+    placement_set = []
+    queue = [[0]]
+
+    while queue:
+        cur = queue.pop(0)
+        if len(cur) == m + 1:
+            cur.pop(0)  # remove the first zero
+            placement_set.append(cur)
+        else:
+            for i in range(cur[-1], n):
+                add = cur[:]
+                add.append(i)
+
+                # check cpu capacity
+                index = len(add) - 2
+                node_capacity = topo.nodes.data()[route[add[index + 1]]]['computing_resource']
+                usage = sfc.vnf_list[index].computing_resource
+                while index > 0 and add[index + 1] == add[index]:
+                    index -= 1
+                    usage += sfc.vnf_list[index].computing_resource
+                if usage <= node_capacity:
+                    queue.append(add)
+
+    configuration_set = []
+    for idx2, placement in enumerate(placement_set):
+        configuration_set.append(Configuration(sfc, route, placement, route_latency, "{}_{}".format(idx, idx2)))
+    return configuration_set
+
+
+# all configuration for one sfc
+def generate_configuration_list(topo: nx.Graph, sfc: SFC) -> List[Configuration]:
+    route_list = generate_route_list(topo, sfc)
+
+    configuration_list = []
+    for idx, item in enumerate(route_list):
+        route, latency = item
+        result = generate_configuration(topo, route, latency, sfc, idx)
+        if result:
+            configuration_list.extend(result)
+
+    print("Size of configuration set: %d" % len(configuration_list))
+
+    return configuration_list
 
 
 # test
--- para_placement/solution.py
+++ para_placement/solution.py
@@ -1,226 +1,6 @@
-import heapq
-
 from pulp import *
 
 from para_placement.model import *
-
-
-class Configuration(BaseObject):
-    def __init__(self, sfc: SFC, route: List[int], place: List[int], route_latency: int, idx: string):
-        self.sfc = sfc
-        self.route = route
-        self.place = place
-        self._route_latency = route_latency
-        self.latency = route_latency + sfc.vnf_latency_sum
-        self.name = "{}_{}".format(sfc.idx, idx)
-
-        # computing resource
-        self.computing_resource = {}
-        for i, vnf in enumerate(sfc.vnf_list):
-            pos = route[place[i]]
-            if pos not in self.computing_resource:
-                self.computing_resource[pos] = 0
-            self.computing_resource[pos] += vnf.computing_resource
-
-        # throughput
-        self.edges = []
-        for i in range(len(route) - 1):
-            start = max(route[i], route[i + 1])
-            end = min(route[i], route[i + 1])
-            self.edges.append("%d:%d" % (start, end))
-
-        self.l = 9999999  # used to find optimal situation
-
-    def __str__(self):
-        return "route: {}\tplace: {}\tcomputing_resource: {}".format(self.route.__str__(), self.place.__str__(),
-                                                                     self.computing_resource.__str__())
-
-    def get_latency(self, para: bool = False) -> float:
-        # todo
-        if para:
-            return 1
-
-        return self._route_latency + self.sfc.vnf_latency_sum
-
-    def computing_resource_ratio(self, topo: nx.Graph):
-        ret = 0
-        for pos in self.computing_resource:
-            ret = max(self.computing_resource[pos] / topo.nodes.data()[pos]['computing_resource'], ret)
-        return ret
-
-    def para_latency_analysis(self) -> List:
-        """
-        Get the optimal parallel execution situation using backtracking
-        """
-        optimal_para = []
-        vnfs = [self.sfc.vnf_list[0]]
-        for i in range(len(self.place) - 1):
-            if self.place[i] == self.place[i + 1]:
-                vnfs.append(self.sfc.vnf_list[i + 1])
-            else:
-                if len(vnfs) > 1:
-                    result = []
-                    self.l = 999999
-                    self.backtracking_analysis(0, vnfs, [], result)
-                    optimal_para.extend(result)
-                    optimal_para.append(0)
-                    vnfs = [self.sfc.vnf_list[i + 1]]
-                else:
-                    optimal_para.append(0)
-                    vnfs = [self.sfc.vnf_list[i + 1]]
-        if len(vnfs) > 1:
-            result = []
-            self.l = 999999
-            self.backtracking_analysis(0, vnfs, [], result)
-            optimal_para.extend(result)
-        return optimal_para
-
-    def backtracking_analysis(self, index, vnfs, analysis_result, result):
-        """
-        for vnf in vnfs, find the optimal parallelism situation and save result in result.
-        len(result) = len(vnfs) - 1
-        result[i] indicates vnfs[i] and vnfs[i+1] whether execute parallelism. 0 for no, 1 for yes
-        """
-        if index == len(vnfs) - 1:
-            # analysis end, compute latency
-            l = 0
-            for vnf in vnfs:
-                l = l + vnf.latency
-            if l < self.l:
-                # find a better situation
-                self.l = l
-                result = analysis_result
-            return
-        if VNF.parallelizable_analysis(vnfs[index], vnfs[index + 1]) >= 0:
-            # parallelizable
-            vnf1 = vnfs.pop(index)
-            vnf2 = vnfs.pop(index)
-            new_vnf = VNF(max(vnf1.latency, vnf2.latency),
-                          vnf1.computing_resource + vnf2.computing_resource,
-                          set.union(vnf1.read_fields, vnf2.read_fields),
-                          set.union(vnf1.write_fields, vnf2.write_fields))
-            vnfs.insert(index, new_vnf)
-            analysis_result.append(1)
-            self.backtracking_analysis(index, vnfs, analysis_result, result)
-            vnfs.pop(index)
-            vnfs.insert(index, vnf2)
-            vnfs.insert(index, vnf1)
-            analysis_result.pop()
-            analysis_result.append(0)
-            self.backtracking_analysis(index + 1, vnfs, analysis_result,
-                                       result)
-            analysis_result.pop()
-        else:
-            analysis_result.append(0)
-            self.backtracking_analysis(index + 1, vnfs, analysis_result,
-                                       result)
-            analysis_result.pop()
-
-        # find the shortest distance to every point
-
-
-def dijkstra(topo: nx.Graph, s: int) -> {}:  # todo
-    ret = {}
-    heap = [(0, s)]
-    while heap:
-        distance, node = heapq.heappop(heap)
-        if node not in ret:
-            ret[node] = distance
-
-        adj_nodes = topo[node]
-        for node in adj_nodes:
-            if node not in ret:
-                new_distance = adj_nodes[node]['latency'] + distance
-                heapq.heappush(heap, (new_distance, node))
-
-    print("Dijkstra: {}".format(ret))
-    return ret
-
-
-# dfs + latency constraint (+ dijkstra)
-def generate_route_list(topo: nx.Graph, sfc: SFC):
-    s = sfc.s
-    d = sfc.d
-
-    shortest_distance = dijkstra(topo, d)
-    if s not in shortest_distance:
-        return []
-
-    stack = [([s], 0)]
-
-    route_set = []
-
-    while stack:
-        route, latency = stack.pop()
-
-        if latency + shortest_distance[route[-1]] > sfc.latency:  # partial latency constraints
-            continue
-
-        if route[-1] == d:
-            route_set.append((route, latency))
-        else:
-            adjacent = topo[route[-1]]
-            for index in adjacent:
-                if index not in route:
-                    new_route = route[:]
-                    new_route.append(index)
-                    stack.append(
-                        (new_route, latency + adjacent[index]['latency']))
-
-    print("Size of path set: %d" % len(route_set))
-
-    return route_set
-
-
-# bfs
-def generate_configuration(topo: nx.Graph, route: List[int], route_latency: int, sfc: SFC, idx: int) \
-        -> List[Configuration]:
-    m = len(sfc.vnf_list)
-    n = len(route)
-
-    placement_set = []
-    queue = [[0]]
-
-    while queue:
-        cur = queue.pop(0)
-        if len(cur) == m + 1:
-            cur.pop(0)  # remove the first zero
-            placement_set.append(cur)
-        else:
-            for i in range(cur[-1], n):
-                add = cur[:]
-                add.append(i)
-
-                # check cpu capacity
-                index = len(add) - 2
-                node_capacity = topo.nodes.data()[route[add[index + 1]]]['computing_resource']
-                usage = sfc.vnf_list[index].computing_resource
-                while index > 0 and add[index + 1] == add[index]:
-                    index -= 1
-                    usage += sfc.vnf_list[index].computing_resource
-                if usage <= node_capacity:
-                    queue.append(add)
-
-    configuration_set = []
-    for idx2, placement in enumerate(placement_set):
-        configuration_set.append(Configuration(sfc, route, placement, route_latency, "{}_{}".format(idx, idx2)))
-    return configuration_set
-
-
-# all configuration for one sfc
-def generate_configuration_list(topo: nx.Graph, sfc: SFC) -> List[Configuration]:
-    route_list = generate_route_list(topo, sfc)
-
-    configuration_list = []
-    for idx, item in enumerate(route_list):
-        route, latency = item
-        result = generate_configuration(topo, route, latency, sfc, idx)
-        if result:
-            configuration_list.extend(result)
-
-    print("Size of configuration set: %d" % len(configuration_list))
-
-    return configuration_list
 
 
 epsilon = 0.03
@@ -375,29 +155,34 @@ def evaluate(model: Model) -> bool:
     return True
 
 
+# Greedy
 def sort_sfcs_by_latency(sfcs):
     for i in range(1, len(sfcs)):
         for j in range(i, 0, -1):
-            if sfcs[j].latency < sfcs[j-1].latency:
-                sfcs[j], sfcs[j-1] = sfcs[j-1], sfcs[j]
+            if sfcs[j].latency < sfcs[j - 1].latency:
+                sfcs[j], sfcs[j - 1] = sfcs[j - 1], sfcs[j]
             else:
                 break
+
 
 def sort_route_list_by_latency(route_list):
     for i in range(1, len(route_list)):
         for j in range(i, 0, -1):
-            if route_list[j][1] < route_list[j-1][1]:
-                route_list[j], route_list[j-1] = route_list[j-1], route_list[j]
+            if route_list[j][1] < route_list[j - 1][1]:
+                route_list[j], route_list[j - 1] = route_list[j - 1], route_list[j]
             else:
                 break
 
-def greedy(model: Model) -> Result:
-    '''Greedy thought: 
+
+def greedy(model: Model):
+    """
+    Greedy thought:
         Sort sfc's latency in increasing order
         For every sfc, sort s to d path's latency in increasing order
         Find the first path whose resources can fulfil sfc requirement
-        Not find then reject! 
-    '''
+        Not find then reject!
+    """
+
     print("Start greedy")
     num_of_accept_sfcs = 0
     greedy_result = []
@@ -419,28 +204,28 @@ def greedy(model: Model) -> Result:
             path, path_latency = route
             place = []
             total_latency = vnf_process_latency + path_latency
-            if total_latency<sfc.latency:
+            if total_latency < sfc.latency:
                 path_index = 0
                 vnf_index = 0
                 while path_index < len(path) and vnf_index < len(sfc.vnf_list):
                     if topo.nodes[path[path_index]]['computing_resource'] >= sfc.vnf_list[vnf_index].computing_resource:
                         place.append(path_index)
                         topo.nodes[path[path_index]]['computing_resource'] -= sfc.vnf_list[vnf_index].computing_resource
-                        vnf_index +=1
+                        vnf_index += 1
                     else:
-                        path_index +=1
+                        path_index += 1
 
                 if vnf_index >= len(sfc.vnf_list):
-                    #we successfully put this sfc in this route
+                    # we successfully put this sfc in this route
                     sfc_reject = 0
-                    num_of_accept_sfcs += 1 
+                    num_of_accept_sfcs += 1
                     greedy_result.append((sfc, 1, path, place))
                 else:
-                    #This route cannot work. add deleted computing resources back
-                    vnf_index -=1
-                    while vnf_index>=0:
+                    # This route cannot work. add deleted computing resources back
+                    vnf_index -= 1
+                    while vnf_index >= 0:
                         topo.node[path[place.pop()]]['computing_resource'] += sfc.vnf_list[vnf_index]
-                        vnf_index -=1
+                        vnf_index -= 1
             if sfc_reject == 0:
                 break
             else:
@@ -449,9 +234,7 @@ def greedy(model: Model) -> Result:
             greedy_result.append((sfc, 0, [], []))
 
     print("Handle finished...")
-    print("Result:", greedy_result) #TODO: show result detail
-
-
+    print("Result:", greedy_result)  # TODO: show result detail
 
 
 def heuristic(model: Model):
