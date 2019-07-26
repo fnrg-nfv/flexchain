a5764badfacb4f89a3cf2cc33e2a110cadcd8769
virgilma <virgilma28@gmail.com>
2019-07-26 15:56:37 +0800

    Update topology.py
    code reformat

--- para_placement/topology.py
+++ para_placement/topology.py
@@ -8,6 +8,7 @@ import networkx as nx
 
 from para_placement.config import TOPO_CONFIG, TOPO_CONFIG2
 from para_placement.helper import extract_str
+from para_placement.model import Model
 
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 fig, ax = plt.subplots()
@@ -204,6 +205,7 @@ def fat_tree_topo(n=4):
 
     return topo
 
+
 def vl2_topo(port_num_of_aggregation_switch=4, port_num_of_tor_for_server=2):
     """Standard vl2 topology
     total port_num_of_aggregation_switch^2 / 4 * port_num_of_tor_for_server servers
@@ -211,7 +213,7 @@ def vl2_topo(port_num_of_aggregation_switch=4, port_num_of_tor_for_server=2):
     topo = nx.Graph()
     num_of_aggregation_switches = port_num_of_aggregation_switch
     num_of_intermediate_switches = num_of_aggregation_switches // 2
-    num_of_ToR_switches = (port_num_of_aggregation_switch // 2) * (port_num_of_aggregation_switch // 2)
+    num_of_tor_switches = (port_num_of_aggregation_switch // 2) * (port_num_of_aggregation_switch // 2)
 
     # create intermediate switch
     for i in range(num_of_intermediate_switches):
@@ -221,21 +223,25 @@ def vl2_topo(port_num_of_aggregation_switch=4, port_num_of_tor_for_server=2):
     for i in range(num_of_aggregation_switches):
         topo.add_node("Aggregation switch {}".format(i), computing_resource=0)
         for j in range(num_of_intermediate_switches):
-            topo.add_edge("Aggregation switch {}".format(i), "Intermediate switch {}".format(j), bandwidth=1000, latency=TOPO_CONFIG2.latency())
+            topo.add_edge("Aggregation switch {}".format(i), "Intermediate switch {}".format(j), bandwidth=1000,
+                          latency=TOPO_CONFIG2.latency())
 
     # create ToR switch
-    num_of_ToR_switches_per_aggregation_switch_can_connect = num_of_aggregation_switches // 2
-    for i in range(num_of_ToR_switches):
+    num_of_tor_switches_per_aggregation_switch_can_connect = num_of_aggregation_switches // 2
+    for i in range(num_of_tor_switches):
         topo.add_node("ToR switch {}".format(i), computing_resource=0)
-        aggregation_index = (i // num_of_ToR_switches_per_aggregation_switch_can_connect) * 2 # every ToR only need to connect 2 aggregation switch
-        topo.add_edge("ToR switch {}".format(i), "Aggregation switch {}".format(aggregation_index), bandwidth=1000, latency=TOPO_CONFIG2.latency())
-        aggregation_index += 1 # The second aggregation switch
-        topo.add_edge("ToR switch {}".format(i), "Aggregation switch {}".format(aggregation_index), bandwidth=1000, latency=TOPO_CONFIG2.latency())
+        # every ToR only need to connect 2 aggregation switch
+        aggregation_index = (i // num_of_tor_switches_per_aggregation_switch_can_connect) * 2
+        topo.add_edge("ToR switch {}".format(i), "Aggregation switch {}".format(aggregation_index), bandwidth=1000,
+                      latency=TOPO_CONFIG2.latency())
+        aggregation_index += 1  # The second aggregation switch
+        topo.add_edge("ToR switch {}".format(i), "Aggregation switch {}".format(aggregation_index), bandwidth=1000,
+                      latency=TOPO_CONFIG2.latency())
         # add server to ToR
         for j in range(port_num_of_tor_for_server):
             topo.add_node("ToR switch {} server {}".format(i, j), computing_resource=TOPO_CONFIG2.cpu())
-            topo.add_edge("ToR switch {} server {}".format(i, j), "ToR switch {}".format(i), bandwidth=1000, latency=TOPO_CONFIG2.latency())
-
+            topo.add_edge("ToR switch {} server {}".format(i, j), "ToR switch {}".format(i), bandwidth=1000,
+                          latency=TOPO_CONFIG2.latency())
 
     topo.name = 'VL2'
 
