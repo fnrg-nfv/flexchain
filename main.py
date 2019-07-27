e640b80b42c32bc594593f420ab946777a24f69e
virgilma <virgilma28@gmail.com>
2019-07-27 14:40:42 +0800

    add alert in main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from para_placement import topology
 from para_placement.helper import *
 from para_placement.solution import *
 import para_placement.config as config
+import winsound
 
 topo_files = ['./gml_data/Cernet.gml', './gml_data/Geant2012.gml', './gml_data/Internetmci.gml']
 
@@ -24,20 +25,23 @@ def main():
 
     iter_times = 10
     unit = 40
+    dup_times = 5
     result = {}
     temple_files = []
 
     for i in range(iter_times):
         size = unit * i + unit
-        model = Model(topo, generate_sfc_list(topo=topo, vnf_set=vnf_set, size=size, base_idx=0))
-        result[size] = iteration(model)
-        temple_files.append("result_{}_{}_{}.pkl".format(model.topo.name, size, current_time()))
-        save_obj(result[size], temple_files[-1])
+        result[size] = []
+        for _ in range(dup_times):
+            model = Model(topo, generate_sfc_list(topo=topo, vnf_set=vnf_set, size=size, base_idx=0))
+            result[size].append(iteration(model))
+            temple_files.append("result_{}_{}_{}.pkl".format(model.topo.name, size, current_time()))
+            save_obj(result[size], temple_files[-1])
 
     filename = "result_{}_{}.pkl".format(model.topo.name, current_time())
     save_obj(result, filename)
 
-    draw_plot(result, "result_{}_{}".format(model.topo.name, current_time()))
+    # draw_plot(result, "result_{}_{}".format(model.topo.name, current_time()))
 
     for temple_file in temple_files:
         os.remove(temple_file)
@@ -73,23 +77,22 @@ def iteration(model: Model):
 
 
 def main_dc():
-    topo = topology.fat_tree_topo(n=5)
+    # topo = topology.fat_tree_topo(n=5)
     # topo = topology.b_cube_topo(k=2)
-    # topo = topology.vl2_topo(port_num_of_aggregation_switch=6, port_num_of_tor_for_server=4)
+    topo = topology.vl2_topo(port_num_of_aggregation_switch=6, port_num_of_tor_for_server=4)
 
     vnf_set = generate_vnf_set(size=30)
-    sfc_size = 100
+    sfc_size = 120
     model = Model(topo, generate_sfc_list2(topo, vnf_set, sfc_size))
     model.draw_topo(1)
 
     Configuration.para = True
     config.DC_CHOOSING_SERVER = False
 
-    iteration(model)
-    # linear_programming(model)
-    # rounding_to_integral(model, rounding_method=rounding_one)
-    #
-    # model.clear()
+    result = iteration(model)
+    filename = "result_{}_{}_{}.pkl".format(model.topo.name, sfc_size, current_time())
+
+    save_obj(result, filename)
 
 
 def add(t1, t2):
@@ -112,5 +115,10 @@ def add(t1, t2):
     return t1 + t2
 
 
+def alert(duration=1000, freq=440):
+    winsound.Beep(freq, duration)
+
+
 if __name__ == '__main__':
-    main()
+    main_dc()
+    alert()
